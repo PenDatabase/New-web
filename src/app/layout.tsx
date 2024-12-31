5124d362677c7e2d55e273650118eb449af7663d
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2024-12-31 18:07:07 +0100

    Done with production

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+import { Header } from "@/sections/Header";
+import { Footer } from "@/sections/Footer";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Light Saas Landing Page",
+  description: "Template created by Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="relative">
+      <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+      <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
+import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <LogoTicker />
+      <ProductShowcase />
+      <Pricing />
+      <Testimonials />
+      <CallToAction />
+    </>
+  );
+}
